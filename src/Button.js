bcfb1fc92a7bfc84e0735018ad60171a2110fbe0
Ned Schwartz <ned@theinterned.net>
2017-07-21 13:58:48 -0400

    adds a bg colour

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,7 @@
 import styled, {css} from 'styled-components/primitives';
 
 const colour = "blue";
+const bgColor = "white";
 
 const ButtonText = styled.Text`
   display: inline-block;
@@ -8,6 +9,7 @@ const ButtonText = styled.Text`
   padding: 10px 30px;
   text-align: center;
   color: ${colour};
+  background-color: ${bgColor};
   border: 2px solid ${colour};
   border-radius: 4px;
 `;
