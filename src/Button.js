b36758cfb1124633bba6cca00559be2800af4987
Ned Schwartz <ned@theinterned.net>
2017-07-21 13:57:38 -0400

    extracts common colour

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,14 @@
-import styled from 'styled-components/primitives';
+import styled, {css} from 'styled-components/primitives';
+
+const colour = "blue";
 
 const ButtonText = styled.Text`
   display: inline-block;
   flex: 1;
   padding: 10px 30px;
   text-align: center;
-  color: blue;
-  border: 2px solid blue;
+  color: ${colour};
+  border: 2px solid ${colour};
   border-radius: 4px;
 `;
 
