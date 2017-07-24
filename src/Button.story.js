b3157bd252ac048da9c6a0f05be9fd4fffba3972
Ned Schwartz <ned@theinterned.net>
2017-07-24 12:18:44 -0400

    adds a story that renders a similar layout to the sketch file

--- src/Button.story.js
+++ src/Button.story.js
@@ -3,24 +3,41 @@ import { storiesOf } from '@storybook/react';
 import styled from 'styled-components/primitives';
 import Button, { submitTheme } from './Button';
 
-const View = styled.View`flex-direction: row;`;
+const Row = styled.View`flex-direction: row;`;
+const Col = styled.View`
+  height: 200px;
+  width: 200px;
+  flex-direction: column;
+  display: flex;
+  justify-content: space-between;
+`;
 
 storiesOf('Button', module)
+  .add('Sketch buttons', () =>
+    <Col>
+      <Row>
+        <Button>A Button</Button>
+      </Row>
+      <Row>
+        <Button theme={submitTheme}>Themed Button</Button>
+      </Row>
+    </Col>
+  )
   .add('A Button', () => <Button>Button</Button>)
   .add('Two Buttons', () =>
-    <View>
+    <Row>
       <Button>A Button</Button>
       <Button>Another Button</Button>
-    </View>
+    </Row>
   )
   .add('A few Buttons', () =>
-    <View>
+    <Row>
       <Button>A Button</Button>
       <Button>Another Button</Button>
       <Button>More Button</Button>
       <Button>So much Button</Button>
       <Button>Give me Button</Button>
-    </View>
+    </Row>
   )
   .add('Styled button', () => {
     return <Button theme={submitTheme}>A styled Button</Button>;
